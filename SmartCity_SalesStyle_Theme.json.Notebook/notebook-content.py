110d401e20d1f3598ba3787ccc991a8d1aed377f
Rupali Singh <100128971+rupali-07@users.noreply.github.com>
2026-01-18 23:26:00 +0530

    Committing 2 items from workspace b6b48a8a-36bd-4726-97e9-a85cbfb2b422

---
+++ SmartCity_SalesStyle_Theme.json.Notebook/notebook-content.py
@@ -0,0 +1,86 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "b1915185-c9fa-4491-9a7f-bdc9eac2f8cb",
+# META       "default_lakehouse_name": "SmartCity_Lakehouse",
+# META       "default_lakehouse_workspace_id": "b6b48a8a-36bd-4726-97e9-a85cbfb2b422",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "b1915185-c9fa-4491-9a7f-bdc9eac2f8cb"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+{
+  "name": "Smart City Dark Sales Theme",
+  "dataColors": [
+    "#38BDF8",
+    "#A78BFA",
+    "#22C55E",
+    "#FACC15",
+    "#F87171"
+  ],
+  "background": "#0F172A",
+  "foreground": "#E5E7EB",
+  "tableAccent": "#38BDF8",
+  "visualStyles": {
+    "*": {
+      "*": {
+        "title": [
+          {
+            "show": true,
+            "fontColor": "#E5E7EB",
+            "fontSize": 12,
+            "fontFamily": "Segoe UI Semibold"
+          }
+        ],
+        "background": [
+          {
+            "show": true,
+            "color": "#020617",
+            "transparency": 0
+          }
+        ],
+        "border": [
+          {
+            "show": true,
+            "color": "#1E293B",
+            "radius": 12
+          }
+        ]
+      }
+    },
+    "card": {
+      "*": {
+        "calloutValue": [
+          {
+            "fontSize": 42,
+            "fontColor": "#38BDF8",
+            "fontFamily": "Segoe UI Bold"
+          }
+        ]
+      }
+    }
+  }
+}
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
